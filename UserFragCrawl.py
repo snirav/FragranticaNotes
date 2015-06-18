a1792860ad671d7f7a96dc40ff405c2a6081a7ab
snirav <snirav@gmail.com>
2015-06-18 10:05:12 -0700

    no structure, but all main code there

---
+++ UserFragCrawl.py
@@ -0,0 +1,26 @@
+__author__ = 'Nirav'
+
+import urllib2
+from bs4 import BeautifulSoup
+import numpy as np
+
+
+userUrl = 'http://www.fragrantica.com/member/614510'
+
+userPerfumes = urllib2.urlopen(userUrl, timeout=10)
+userPerfumeSoup = BeautifulSoup(userPerfumes)
+perfumeHas = [int(a.attrs['id'][2:]) for a in userPerfumeSoup.findAll('ul', attrs={'id': 'imam-sortable'})[0].findAll('li')]
+perfumeWants = [int(a.attrs['id'][2:]) for a in userPerfumeSoup.findAll('ul', attrs={'id' :'zelim-sortable'})[0].findAll('li')]
+perfumeHad = [int(a.attrs['id'][2:]) for a in userPerfumeSoup.findAll('ul', attrs={'id': 'imao-sortable'})[0].findAll('li')]
+
+userLoveHateUrl = 'http://www.fragrantica.com/ajax.php?view=emotion&id=614510'
+userLoveHatePerfumes = urllib2.urlopen(userLoveHateUrl, timeout=10)
+userLoveHateSoup = BeautifulSoup(userLoveHatePerfumes)
+loveHateList = [[],[],[]]
+lovelikedislikeCount = -1
+for loveHate in userLoveHateSoup.findAll('body')[0]:
+    if loveHate.name == 'h3':
+        lovelikedislikeCount += 1
+    elif loveHate.name == 'a':
+        if loveHate.attrs['href'] != None:
+            loveHateList[lovelikedislikeCount].append(int(loveHate.attrs['href'].split('.')[0].split('-')[-1]))

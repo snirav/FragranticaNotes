feb3d2e2c74d5fd03b97f92dd00b8f63798f0480
snirav <snirav@gmail.com>
2015-06-18 10:08:46 -0700

    no structure, but all main code there

---
+++ FragCrawlThreadCreation.py
@@ -0,0 +1,92 @@
+import threading
+import urllib2
+from bs4 import BeautifulSoup
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Spider class for running the 
+class mySpider(threading.Thread):
+    def __init__(self,siteUrl,designerName):
+        threading.Thread.__init__(self)
+        self.siteUrl = siteUrl
+        self.designerName = designerName
+    def run(self):
+        print "Starting " + self.designerName
+        getNextlevelUrls(self.siteUrl,self.designerName)
+        print "Exiting " + self.designerName
+        
+def getNextlevelUrls(siteUrl,designerName):
+    #f=codecs.open('C:\\Perfumes\\'+designerName+'.txt','w',encoding='utf8')
+    try:
+        designerResponse = urllib2.urlopen(siteUrl+'/designers/'+designerName+'.html',timeout=10)
+    except urllib2.URLError, e:
+        print "Oops, timed out?",e
+    designerSoup = BeautifulSoup(designerResponse)
+    links = designerSoup.findAll('a')
+    perfumeCount = 0
+    print designerName
+    #f.write(designerName +'\n')
+    for link in links:
+        if link.get('href')!=None:
+            if link['href'].find('/perfume/'+designerName) != -1:
+
+                perfumeCount += 1
+
+                perfumeID = str(link['href'].split('-')[-1].split('.')[0])
+                print(perfumeID)
+                print(link.img.get('alt').split(' ', 1)[1])
+                userListUrl = siteUrl+'/'+ '/ajax.php?view=whoHasHadWant&perfume_id=' + perfumeID
+                try:
+                    userListResponse = urllib2.urlopen(userListUrl,timeout=10)
+                except urllib2.URLError, e:
+                    print "URL Fetch time-out",e
+                userListSoup = BeautifulSoup(userListResponse)
+                perfumeWhoHasHadSigWantCurr = [int(tag.string) for tag in (userListSoup.find('p').findAll('b'))]
+                print perfumeWhoHasHadSigWantCurr
+
+                perfumeUrl = siteUrl+'/'+link['href']
+                try:
+                    perfumeResponse = urllib2.urlopen(perfumeUrl,timeout=10)
+                except urllib2.URLError, e:
+                    print "URL Fetch time-out",e
+                perfumeSoup = BeautifulSoup(perfumeResponse)
+
+                perfumeNotesTitle = perfumeSoup.findAll('span',attrs={'class':'rtgNote'})
+                noteList = [str(perfumeTitle.img.get('alt')) for perfumeTitle in perfumeNotesTitle]
+                noteID = [str(perfumeTitle.attrs['title']) for perfumeTitle in perfumeNotesTitle]
+                print(noteList)
+                print(noteID)
+                perfumeMenWomen = np.sign(np.array([perfumeSoup.title.text.find(' men'), perfumeSoup.title.text.find(' women')]))
+                print(perfumeMenWomen)
+                perfumeVotes = perfumeSoup.findAll('table',attrs={'class':'voteLS'})
+                perfumeLongevityVotes = np.array([int(a.string) for a in perfumeVotes[0].findAll('td', attrs={'class':'ndSum'})])
+                print(perfumeLongevityVotes)
+                perfumeSillageVotes = np.array([int(a.string) for a in perfumeVotes[1].findAll('td', attrs={'class':'ndSum'})])
+                print(perfumeSillageVotes)
+                perfumeAttrs = np.append(perfumeMenWomen, np.append(perfumeLongevityVotes, perfumeSillageVotes))
+                perfumeNotes = perfumeSoup.findAll('div',attrs={'id':'userMainNotes'})
+
+                perfumeNotesStr = str(perfumeNotes[0].attrs[u'title'])
+                notesVec = np.zeros(350, dtype = int)
+                perfumeNotesStrSplit = perfumeNotesStr.split(';')
+                for notes in perfumeNotesStrSplit :
+                    individualNotes = notes.split(':')
+                    individualNotesInt = np.array([int(str_tmp) for str_tmp in individualNotes])
+                    notesVec[individualNotesInt[0]] = individualNotesInt[1]
+                print(notesVec)
+
+def crawl(url,designer,designerList):
+    threads=[]
+    if designer=='All':
+        for designer in designerList:
+            try:
+                t = mySpider(url,designer)
+                threads.append(t)
+            except Exception as errtxt:
+                print errtxt
+    else:
+            getNextlevelUrls(url,designer)
+    [t.start() for t in threads]
+    [t.join() for t in threads]
+    print "Main Thread Completed!!" 
---
+++ FragranceCrawlerApp.py
@@ -0,0 +1,117 @@
+import Tkinter as tk
+import tkMessageBox
+from FragCrawlThreadCreation import *
+
+class fragranceCrawler(tk.Frame):
+    def __init__(self, master):
+        # Initialize window using the parent's constructor
+        tk.Frame.__init__(self,
+                          master,
+                          width=400,
+                          height=300)
+        # Set the title
+        self.master.title('Perfume Crawler')
+ 
+        # This allows the size specification to take effect
+        self.pack_propagate(0)
+ 
+        # We'll use the flexible pack layout manager
+        self.pack()
+ 
+        #Fill the  Website List
+        self.webSiteList=['http://www.fragrantica.com']
+        #Fill the city List
+        self.designerEntries=['Azzaro',
+                            'Bond-No-9',
+                            'Boucheron',
+                            'Britney-Spears',
+                            'Burberry',
+                            'Bvlgari',
+                            'Cacharel',
+                            'Calvin-Klein',
+                            'Carolina-Herrera',
+                            'Cartier',
+                            'Chanel',
+                            'Creed',
+                            'Davidoff',
+                            'Dior',
+                            'Dolce%26Gabbana',
+                            'Donna-Karan',
+                            'Elizabeth-Arden',
+                            'Escada',
+                            'Estee-Lauder',
+                            'Giorgio-Armani',
+                            'Givenchy',
+                            'Gucci',
+                            'Guerlain',
+                            'Guy-Laroche',
+                            'Gwen-Stefani',
+                            'Hermes',
+                            'Hugo-Boss',
+                            'Jean-Paul-Gaultier',
+                            'Jennifer-Lopez',
+                            'Kenzo',
+                            'Lacoste',
+                            'Lancome',
+                            'Liz-Claiborne',
+                            'Mariah-Carey',
+                            'Moschino',
+                            'Nina-Ricci',
+                            'Paco-Rabanne',
+                            'Paris-Hilton',
+                            'Perry-Ellis',
+                            'Prada',
+                            'Ralph-Lauren',
+                            'Rochas',
+                            'Thierry-Mugler',
+                            'Tommy-Hilfiger',
+                            'Vera-Wang',
+                            'Versace',
+                            'Victoria%60s-Secret',
+                            'Yves-Saint-Laurent']
+        # Website Selection Menu
+        self.defaultWebsite = tk.StringVar()
+        self.defaultWebsite.set('Please Select the Websites to crawl')
+        self.websiteDropdown = tk.OptionMenu(self,
+                                      self.defaultWebsite,*self.webSiteList)
+                                     
+        # Designer Selection Menu
+        self.defaultDesigner = tk.StringVar()
+        self.defaultDesigner.set('Please Select the sites to crawl')
+        self.designerList = tk.OptionMenu(self,
+                                      self.defaultDesigner,"All",
+                                      *self.designerEntries)
+ 
+        
+        # Declaring the buttons and linking the functions
+        self.textBox=tk.Text(xscrollcommand=set(),yscrollcommand=set(),height=5,width=5)
+        
+        self.crawl_button = tk.Button(self,
+                                   text='Crawl Designer',
+                                   command=self.initiateCrawl,height=2,width=15)
+        
+
+       
+        
+       
+        # Put the controls on the form
+        
+        self.websiteDropdown.pack(fill=tk.X, side=tk.TOP)
+        self.designerList.pack(fill=tk.X,side=tk.TOP)
+        self.crawl_button.pack(fill=tk.X,side=tk.TOP)
+        self.textBox.pack(fill=tk.X,side=tk.TOP)
+        
+        
+     # Crawl handling  routine 
+    def initiateCrawl(self):                     
+        crawl(self.defaultWebsite.get().title().lower(),self.defaultDesigner.get().title(),self.designerEntries)
+        tkMessageBox.showinfo("Crawling Status","Crawling Completed Successfully!!")
+        self.textBox.insert('1.0',"Crawling Completed Successfully!!","a")
+     
+
+    def run(self):
+        ''' Run the app '''        
+        self.mainloop()
+ 
+app = fragranceCrawler(tk.Tk())
+app.run()
